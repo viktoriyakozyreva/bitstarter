6f97596d310c42e748b31ac95804986c14cf6b62
Victoria Kozyreva <viktoriya.kozyreva@gmail.com>
2013-07-11 20:24:50 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+
     var buf = fs.readFileSync('index.html');
     var str = buf.toString('utf8', 0);
     response.send( str );

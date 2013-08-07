ae77396786898ae601baa16817d67b8240c25964
Victoria Kozyreva <viktoriya.kozyreva@gmail.com>
2013-08-07 13:19:30 +0000

    Fixed staging bugs in develop branch

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
 
     var buf = fs.readFileSync('index.html');
@@ -9,7 +11,8 @@ app.get('/', function(request, response) {
     response.send( str );
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
